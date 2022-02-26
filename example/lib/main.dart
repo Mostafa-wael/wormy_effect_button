12b3457edfbdf94ebce9773643468a3b4403bd10
Mostafa wael <mostafa.w.k000@gmail.com>
2022-02-26 22:32:10 +0200

    working!!

---
+++ example/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:draggable_floating_action_button/draggable_floating_action_button.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: DraggableFloatingActionButton(
+        onPressed: _incrementCounter,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            child: const Icon(Icons.camera),
+            decoration:
+                const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            child: const Icon(Icons.camera),
+            decoration:
+                const BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            child: const Icon(Icons.camera),
+            decoration: const BoxDecoration(
+                color: Colors.green, shape: BoxShape.circle),
+          ),
+        ],
+        delayBias: 200,
+      ),
+    );
+  }
+}
--- lib/draggable_floating_action_button.dart
+++ lib/draggable_floating_action_button.dart
@@ -1,7 +1,120 @@
 library draggable_floating_action_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// draggable floating action button
+class DraggableFloatingActionButton extends StatefulWidget {
+  final List<Widget> children; // the widget that will be animated
+  final Offset initialOffset;
+  final Function onPressed;
+  final int delayBias;
+
+  // ignore: use_key_in_widget_constructors
+  const DraggableFloatingActionButton({
+    Key? key,
+    required this.children,
+    this.initialOffset = const Offset(340, 610),
+    required this.onPressed,
+    required this.delayBias,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _DraggableFloatingActionButtonState();
+}
+
+class _DraggableFloatingActionButtonState
+    extends State<DraggableFloatingActionButton> {
+  bool _isDragging = false;
+  late Offset _offset;
+  late Offset _minOffset;
+  late Offset _maxOffset;
+
+  @override
+  void initState() {
+    // Set initial offset
+    super.initState();
+    _offset = widget.initialOffset;
+    WidgetsBinding.instance?.addPostFrameCallback(_setBoundary);
+  }
+
+  void _setBoundary(_) {
+    try {
+      setState(() {
+        _minOffset = const Offset(0, 0);
+        _maxOffset = Offset(MediaQuery.of(context).size.width,
+            MediaQuery.of(context).size.height);
+      });
+    } catch (e) {
+      print('catch: $e');
+    }
+  }
+
+  void _updatePosition(
+    PointerEvent pointerMoveEvent,
+  ) {
+    var newOffsetX = _offset.dx + pointerMoveEvent.delta.dx;
+    var newOffsetY = _offset.dy + pointerMoveEvent.delta.dy;
+
+    // ensure posistion within limits
+    if (newOffsetX < _minOffset.dx) {
+      newOffsetX = _minOffset.dx;
+    } else if (newOffsetX > _maxOffset.dx) {
+      newOffsetX = _maxOffset.dx;
+    }
+    if (newOffsetY < _minOffset.dy) {
+      newOffsetY = _minOffset.dy;
+    } else if (newOffsetY > _maxOffset.dy) {
+      newOffsetY = _maxOffset.dy;
+    }
+    // update position
+    setState(() {
+      _offset = Offset(newOffsetX, newOffsetY);
+    });
+  }
+
+  List<Widget> getPositions() {
+    final children = <Widget>[];
+    final len = widget.children.length;
+    for (var i = 0; i < len; i++) {
+      children.add(
+        AnimatedPositioned(
+          curve: Curves.fastLinearToSlowEaseIn,
+          left: _offset.dx,
+          top: _offset.dy,
+          duration: Duration(
+              milliseconds: i * widget.delayBias + i * widget.delayBias * 2),
+          child: Listener(
+            onPointerMove: (PointerEvent details) {
+              _updatePosition(details);
+              setState(() {
+                _isDragging = true;
+              });
+            },
+            onPointerUp: (PointerEvent details) {
+              print('onPointerUp');
+              if (_isDragging) {
+                setState(() {
+                  _isDragging = false;
+                  _offset = widget.initialOffset; // return to the origin
+                });
+              } else {
+                // the button was clicked not dragged
+                widget.onPressed();
+              }
+            },
+            child: Container(
+              key: ValueKey(i),
+              child: widget.children[i],
+            ),
+          ),
+        ),
+      );
+    }
+    return List.from(children.reversed); // to keep the order order
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: getPositions());
+  }
 }
